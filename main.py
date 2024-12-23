6e6701373c0cab2d5c33915f13acf3485a050cff
RandaeGS <garciasanchezrd@gmail.com>
2024-12-23 11:04:32 -0400

    count characters

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ def main():
     with open("./books/frankenstein.txt") as f:
         file_contents = f.read()
         print(word_cound(file_contents))
+        print(character_count(file_contents))
+
+def character_count(file):
+    file = file.lower()
+    characters = {}
+    for ch in file:
+        if ch in characters:
+            characters[ch] += 1
+        else:
+            characters[ch] = 1
+
+    return characters
 
 def word_cound(file):
     return len(file.split())
