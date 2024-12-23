42b2d374341a5fff4ca26c80444520c4fb40a22c
RandaeGS <garciasanchezrd@gmail.com>
2024-12-23 10:50:12 -0400

    count words

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def main():
     with open("./books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        print(word_cound(file_contents))
+
+def word_cound(file):
+    return len(file.split())
+
 
 main()
